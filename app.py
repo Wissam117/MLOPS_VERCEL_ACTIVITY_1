a9ae5781b6b371469bd4c9763113e1624e7e1229
Wissam <mwissam11@gmail.com>
2025-02-19 15:51:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Deploying Flask App at Vercel"
+    return "Deploying Flask App at Vercell"
 
 if __name__ == '__main__':
     app.run(debug=True)
