3a646060f730ed30964b02e6e4f6628bdd737205
Wissam <mwissam11@gmail.com>
2025-03-23 12:23:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Deploying Flask App at Vercel! Wissam did it with the help of Umar and Saad'
+    return 'Deploying Flask App at Vercel, I did! Wissam did it with the help of Umar and Saad'
 
 if __name__ == '__main__':
     app.run(debug=True)
